4940f1ce989e31cd5319c0fe9fc098952e66911d
Marco <boett.m@gmail.com>
2020-12-06 21:28:54 +0100

    Mel-filterbanks work in progress: filtraggio e calcolo log(E) completato

--- framing.py
+++ framing.py
@@ -60,7 +60,7 @@ def main():
     mel_step = (f_max_mel - f_min_mel) / (n_thresh - 1)
     mel_vec = np.zeros(n_thresh)            # Thresholds in mel-space
     freq_vec = np.zeros(n_thresh)           # Equivalent thresholds' freq.
-    freq_idx = np.zeros(n_thresh)
+    freq_idx = np.zeros(n_thresh, dtype=int)
 
     for i in range(n_thresh):
         mel_vec[i] = f_min_mel + i * mel_step
@@ -72,10 +72,16 @@ def main():
 
     # Creo ora una matrice le cui righe siano i valori effettivi di ogni filtro
 
-    mel_filterbanks = []
+    mel_filterbanks = np.zeros((periodogram_mat.shape[1], n_fb))
     for i in range(n_fb):
         up_step = freq_idx[i + 1] - freq_idx[i] + 1
-        down_step = freq_idx[i + 2] - freq_idx[i + 1]
+        mel_filterbanks[freq_idx[i]:freq_idx[i + 1] + 1, i] = np.linspace(0.0, 1.0, num=up_step)
+        down_step = freq_idx[i + 2] - freq_idx[i + 1] + 1
+        mel_filterbanks[freq_idx[i + 1]:freq_idx[i + 2] + 1, i] = np.linspace(1.0, 0.0, num=down_step)
+
+    # Ogni colonna è un filterbank, quindi basta un prodotto tra matrici pe applicare i filti e sommare i valori
+    # Apply mel_filterbanks and compute log(E)
+    log_mat = np.log(np.matmul(periodogram_mat, mel_filterbanks))
 
     z = 1       # Linea di debugging
 
