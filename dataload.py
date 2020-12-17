35147e00e109435457ac0e62577c656ae8ab8d1a
Marco <boett.m@gmail.com>
2020-12-17 19:10:02 +0100

    Creazione csv del dataset. Esperimenti con il formato del dataframe.

--- dataload.py
+++ dataload.py
@@ -12,22 +12,34 @@ def main():
 
     data_dir = "dataset\\speech_commands_v0.02"
     all_dir = np.array(listdir(data_dir))
-    data = pd.DataFrame(columns=['filename', 'data', 'fs', 'label'])
-
+    data = pd.DataFrame(columns=['filename', 'data', 'label'])
+    fs = 16000
+    dur = 0.025
+    step = 0.010
     all_time = time.time()
-    for dir in all_dir[:-1]:
-        all_files = np.array(listdir(data_dir+"\\"+dir))
+    for d in all_dir[:-1]:
+        all_files = np.array(listdir(data_dir+"\\"+d))
         start_time = time.time()
-        data_temp = pd.DataFrame(columns=['filename', 'data', 'fs', 'label'])
-        fs = wave.open(data_dir + "\\" + dir + "\\" + all_files[0], 'rb').getframerate()
+        data_temp = pd.DataFrame(columns=['filename', 'data', 'label'])
+        #fs = wave.open(data_dir + "\\" + dir + "\\" + all_files[0], 'rb').getframerate()
         for f in all_files:
-            src = read(data_dir+"\\"+dir+"\\"+f)
-            temp = pd.DataFrame([[dir+"/"+f, np.array(src[1], dtype=float), fs, dir]], columns=['filename', 'data', 'fs', 'label'])
+            src = read(data_dir+"\\"+d+"\\"+f)
+            src = framing.framing(np.array(src[1], dtype=int), fs, dur, step)
+            temp = pd.DataFrame([[d+"/"+f, src, d]], columns=['filename', 'data', 'label'])
             data_temp = data_temp.append(temp, ignore_index=True)
         data = pd.concat([data, data_temp], ignore_index=False)
-        print(dir + ": --- %s seconds ---" % (time.time() - start_time))
+        print(d + ": --- %s seconds ---" % (time.time() - start_time))
 
     print(": --- %s seconds ---" % (time.time() - all_time))
+    prova = data['data'][0]
+
+    data.to_csv("dataframe.csv")
+    all_time = time.time()
+    data = pd.read_csv("dataframe.csv")
+    prova = data['data'][0]
+    print(": --- %s seconds ---" % (time.time() - all_time))
+
+
     z = 1  # Linea di debugging
 
 if __name__ == "__main__":
