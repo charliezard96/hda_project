5101dc9b3d18c52c713ed23beb840db8e44184c0
charliezard96 <carloalberto96@gmail.com>
2021-01-26 18:14:59 +0100

    import

--- autoencoder.py
+++ autoencoder.py
@@ -1,11 +1,16 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
-import numpy as np
-from sklearn.model_selection import train_test_split
-from lfw_dataset import load_lfw_dataset
+from tensorflow.keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from tensorflow.keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing import image
+from IPython.display import SVG
+from kt_utils import *
+
 import matplotlib.pyplot as plt
-import keras_utils
-import numpy as np
+from matplotlib.pyplot import imshow
+from IPython.display import Image
 
 %matplotlib inline
 np.random.seed(1)
