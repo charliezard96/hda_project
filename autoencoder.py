27b1d191860483707a22a766300ced1acfa1e5ad
Marco <boett.m@gmail.com>
2021-01-26 18:08:35 +0100

    Add import

--- autoencoder.py
+++ autoencoder.py
@@ -1,1 +1,11 @@
-#Prova
+import tensorflow as tf
+from tensorflow.keras import layers
+import numpy as np
+from sklearn.model_selection import train_test_split
+from lfw_dataset import load_lfw_dataset
+import matplotlib.pyplot as plt
+import keras_utils
+import numpy as np
+
+%matplotlib inline
+np.random.seed(1)
