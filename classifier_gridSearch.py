f1c91201b829e7ffcccf265a1f15b9eb3c6adace
Marco <boett.m@gmail.com>
2021-02-18 12:26:05 +0100

    Classifier grid search setup

---
+++ classifier_gridSearch.py
@@ -0,0 +1,91 @@
+import numpy as np
+import h5py
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, Reshape, Conv2DTranspose, ReLU, Cropping2D
+from sklearn import decomposition, model_selection, metrics, manifold
+
+from tensorflow.keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing import image
+from IPython.display import SVG
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from IPython.display import Image
+import datamerge
+import pandas as pd
+
+def extractData(dataset_raw):
+    data = dataset_raw.data.to_frame().applymap(lambda x: list(x[:, :12].flatten()))
+    data['label'] = dataset_raw.label.to_numpy()
+
+    # Create label dictionaries (35 different known words)
+    com_labels = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go']  # Command labels
+    label_dict = dict(zip(com_labels, range(len(com_labels))))  # Commands oriented classification
+
+
+    samples = data.data.tolist()
+    samples = np.array(samples)
+    samples = samples.reshape((-1, 100, 12, 1))
+
+    true_labels = data['label'].to_frame().applymap(lambda x: label_dict.get(x, len(label_dict))).to_numpy()
+    return(data, samples, true_labels)
+
+
+def Classifier2L(input_shape, l1=1024, l2=2048):
+
+    X_input = Input(input_shape)
+    # Linear layers
+    X = Dense(l1, activation='relu', name='linear0')(X_input)
+    X = Dense(l2, activation='relu', name='linear1')(X)
+    X = Dense(11, activation='softmax', name='out')(X)
+
+    model = Model(inputs=X_input, outputs=X, name='Classifier')
+    return model
+
+def main():
+
+    train_dataset_raw = datamerge.importDataset('dTrain2')
+    #train_dataset_raw = pd.read_hdf('dVal.h5')
+
+    print('### Train data processing ###')
+    data, samples, true_labels = extractData(train_dataset_raw)
+
+    print('### Validation data processing ###')
+    val_dataset_raw = pd.read_hdf('dVal.h5')
+    val_data, val_samples, val_labels = extractData(val_dataset_raw)
+
+
+    hyparams = [2**i for i in range(9,12)]
+
+
+    # Model
+    print('Model initialization')
+    encoder = tf.keras.models.load_model('autoencoders_models\\encoder_gpu_150_noise.h5')
+    #print(encoder.summary())
+    in_shape = (100, 12, 1)
+    inp = tf.keras.Input(in_shape)
+    features = encoder(inp)
+    n_features = 256
+
+    train_name = 'train_loss'
+    val_name = 'val_loss'
+    for l2 in hyparams:
+        for l1 in hyparams:
+
+            classification = Classifier2L(n_features, l1, l2)
+            prediction = classification(features)
+
+            predictor = tf.keras.Model(inputs=inp, outputs=prediction)
+            predictor.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+            history = predictor.fit(x=samples, y=true_labels, validation_data=(val_samples, val_labels),
+                                epochs=2, batch_size=256)
+
+
+
+    z = 1  # Linea di debugging
+
+
+if __name__ == "__main__":
+    main()
---
+++ dataloadV2.py
@@ -0,0 +1,90 @@
+import wave
+from scipy.io.wavfile import read
+import numpy as np
+import pandas as pd
+from os import listdir
+import time
+import framing
+import math
+import tables
+
+
+def main():
+
+    data_dir = "dataset\\speech_commands_v0.02"
+    test = "dataset\\testing_list.txt"
+    val = "dataset\\validation_list.txt"
+    dest_dir = 'dTrainV2_2'
+    all_dir = np.array(listdir(data_dir))
+    test_set = np.loadtxt(test, dtype=str)
+    val_set = np.loadtxt(val, dtype=str)
+
+    fs = 16000      # Sampling frequency (better time performances than extract it from the file)
+    dur = 0.025     # Frame duration in ms
+    step = 0.010    # Frame step size in ms
+    n_mel = 26
+    f_min = 300
+    all_time = time.time()      # Used to evaluate time performances
+    #data_train = pd.DataFrame(columns=['filename', 'data', 'label'])
+    data_val = pd.DataFrame(columns=['filename', 'data', 'label'])
+    data_test = pd.DataFrame(columns=['filename', 'data', 'label'])
+    #data.to_hdf('dataframe.h5', key='df', mode='w')
+    mel_mat = framing.getMellFilterbanks(fs*dur, fs/2, f_min, n_mel)
+
+    # Labels processing
+    com_labels = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go']
+    label_dict = dict(zip(com_labels, range(len(com_labels))))
+
+
+    for d in all_dir[:-1]:
+        all_files = np.array(listdir(data_dir+"\\"+d))
+        start_time = time.time()        # Used to evaluate time performances
+        data_temp = pd.DataFrame(columns=['filename', 'label', 'label_num', 'data'])     # Initialize sub-dataframe
+        for f in all_files:
+            # Read and process the sample
+            src = read(data_dir+"\\"+d+"\\"+f)
+            src = framing.fullFeatExtraction(np.array(src[1], dtype=float), mel_mat, fs, dur, step)
+            if src.shape[0] != 100:
+                continue
+            # Remove energy values and convert to list
+            src = list(src[:, :-3].flatten())
+            lab = label_dict.get(d, len(label_dict))
+            # Update sub-dataframe
+            temp = pd.DataFrame([[d+"/"+f, d, lab, src]], columns=['filename', 'label', 'label_num', 'data'])
+            data_temp = data_temp.append(temp, ignore_index=True)
+        # Extract test samples
+        idx_t = data_temp.index[np.in1d(data_temp['filename'], test_set)]
+        data_temp_t = data_temp.iloc[idx_t]
+        # Extract test samples
+        idx_v = data_temp.index[np.in1d(data_temp['filename'], val_set)]
+        data_temp_v = data_temp.iloc[idx_v]
+        # Remove test and validation set
+        data_temp = data_temp.drop(idx_t)
+        data_temp = data_temp.drop(idx_v)
+        data_temp.to_hdf(dest_dir+'\\'+d+'.h5', key='df', mode='w')
+
+        #data_temp.to_hdf('dataframe.h5', key='df', mode='a', format='table', append=True)
+        # Update all the sets
+        data_test = pd.concat([data_test, data_temp_t], ignore_index=False)
+        data_val = pd.concat([data_val, data_temp_v], ignore_index=False)
+        #data_train = pd.concat([data_train, data_temp], ignore_index=False)
+        print(d + ": --- %s seconds ---" % (time.time() - start_time))
+
+    # Save all the sets
+    #data_test.to_pickle('dTest.pkl')
+    #data_val.to_pickle('dVal.pkl')
+    data_test.to_hdf('dTestV2.h5', key='df', mode='w')
+    data_val.to_hdf('dValV2.h5', key='df', mode='w')
+    #data_train.head(math.floor(data_train.shape[0]/2)).to_hdf('dTrain1.h5', key='df', mode='w')
+    #data_train.tail(math.ceil(data_train.shape[0]/2)).to_hdf('dTrain2.h5', key='df', mode='w')
+    print("Dataset file creation: --- %s seconds ---" % (time.time() - all_time))
+    all_time = time.time()
+    # Reload train set
+    #data_train = pd.read_hdf('dTrain1.h5', key='df')
+    #data_train = pd.concat([data_train, pd.read_hdf('dTrain2.h5', key='df')], ignore_index=False)
+    print("Load dataset file: --- %s seconds ---" % (time.time() - all_time))
+
+    z = 1  # Linea di debugging
+
+if __name__ == "__main__":
+    main()
